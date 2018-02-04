e27f64f7e5b23559287e06789c1cc625529ffb82
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2018-02-04 12:30:37 -0500

    Lab2 final
    Added Script. Added RigidBody 2D and integrated with button sprites and

---
+++ COMP305-W2018/Assets/Scripts/ChangeCharacterColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCharacterColor : MonoBehaviour {
+	public Sprite Sprite;
+	public GameObject character;
+	private SpriteRenderer sRend;
+	// Use this for initialization
+	private void Start () {
+		 sRend = character.GetComponent<SpriteRenderer>();
+	}
+
+	 void OnMouseDown()
+	{
+		sRend.sprite = Sprite;
+	}
+}
